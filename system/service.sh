102fafffbc40d0c9cdc5b6b6a571d7c2032261d7
yancyyu <1191500820@qq.com>
2020-05-24 13:01:48 +0800

    change service.sh

--- system/service.sh
+++ system/service.sh
@@ -1,3 +1,26 @@
-!/bin/bash
+#!/usr/bin/bash
 cd ../public
-php -S 127.0.0.1:8001 nohup command &
+LISINING_HOST='127.0.0.1'
+LISINING_PORT='8001'
+php -S ${LISINING_HOST}:${LISINING_PORT} nohup command & >>out.txt 2>&1
+
+ti1=`date +%s`    #获取时间戳
+ti2=`date +%s`
+i=$(($ti2 - $ti1 ))
+
+while [[ "$i" -ne "1" ]]
+do
+	ti2=`date +%s`
+	i=$(($ti2 - $ti1 ))
+done
+
+swoole_woker=`netstat -tunlp| grep ${LISINING_PORT}`
+echo ${swoole_woker}
+if [ ! -z "${swoole_woker}" ]; then
+echo successfull
+echo -e '\003:'
+else
+echo start fail
+echo -e '\003:'
+fi
+
